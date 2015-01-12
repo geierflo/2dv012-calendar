fa0dcdf1800d621d0465c8b6a321144340f70d98
jm222hw@student.lnu.se <jm222hw@student.lnu.se@d48fed7d-0b3d-e30f-9b0f-b7e170449b4e>
2015-01-12 21:12:33 +0000

    admin check fixed

--- Calendar-2dv012/src/beans/UserBean.java
+++ Calendar-2dv012/src/beans/UserBean.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.ConfigurableNavigationHandler;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,6 +33,20 @@ public class UserBean implements Serializable {
 	private String role;
 	private User loggedInUser;
 	private String url;
+	
+	public void isAdmin(ComponentSystemEvent event){
+		 
+		FacesContext fc = FacesContext.getCurrentInstance();
+	 
+		if (!loggedInUser.getRole().contentEquals("Admin")){
+	 
+			ConfigurableNavigationHandler nav 
+			   = (ConfigurableNavigationHandler) 
+				fc.getApplication().getNavigationHandler();
+	 
+			nav.performNavigation("noaccess");
+		}		
+	  }	
 
 
 	public String getUrl() {
